86f2d42395b46f414bd62726e7b5c5b917f9342f
Kyler Ramsey <104465783+kylerramsey@users.noreply.github.com>
2022-06-27 18:46:10 -0400

    Delete today directory

--- today/main.py
+++
@@ -1,5 +0,0 @@
-from shopping_cart import Shop
-
-if __name__ == '__main__':
-    shopping_session = Shop()
-    shopping_session.run()
--- today/try_except_finally.py
+++
@@ -1,7 +0,0 @@
-# f = open('new_file.')
-try:
-    f = open('new_file.pp', 'r')
-except Exception as err:
-    print(err)
-finally:
-    f.close()
